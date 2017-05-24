bc8bb2617302685ae29666a95646a116052bba47
Erik <elidorian@gmail.com>
2017-05-24 22:07:01 +0200

    Started Java Code

--- app/src/main/java/com/example/eslowikowski/dicee/MainActivity.java
+++ app/src/main/java/com/example/eslowikowski/dicee/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.eslowikowski.dicee;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button rollButton;
+        rollButton = (Button) findViewById(R.id.rollButton);
+
+        ImageView leftDice = (ImageView) findViewById(R.id.image_leftdice);
+        ImageView rightDice = (ImageView) findViewById(R.id.image_rightdice);
+
     }
 }
